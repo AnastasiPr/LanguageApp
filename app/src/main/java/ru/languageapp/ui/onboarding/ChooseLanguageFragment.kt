0251598cd396a9e05572e0b0c2000230d1be9a1a
‘AnastasiPr’ <ana.steshaa10@gmail.com>
2024-05-16 21:57:27 +0300

    first commit

---
+++ app/src/main/java/ru/languageapp/ui/onboarding/ChooseLanguageFragment.kt
@@ -0,0 +1,46 @@
+package ru.languageapp.ui.onboarding
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
+import ru.languageapp.databinding.FragmentChooseLanguageBinding
+import ru.languageapp.databinding.FragmentConnectionBinding
+import ru.languageapp.databinding.FragmentSecondBinding
+
+/**
+ * A simple [Fragment] subclass as the second destination in the navigation.
+ */
+class ChooseLanguageFragment : Fragment() {
+
+    private var _binding: FragmentChooseLanguageBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        _binding = FragmentChooseLanguageBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.russianButton.setOnClickListener {
+
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
